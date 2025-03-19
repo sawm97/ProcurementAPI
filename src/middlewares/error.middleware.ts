a9b5861f7a46a53a8b6ac7c8842bec25b55eefc2
Sri Arief Wibawa Mukti <sriariefwibawamukti@gmail.com>
2025-03-19 11:01:44 +0700

    adding error handling

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,31 @@
+import { error } from "console";
+import { NextFunction, Request, Response } from "express";
+
+export class HttpExeception extends Error {
+    public status: number;
+    public message: string;
+
+    constructor(status: number, message: string) {
+        super(message);
+        this.status = status;
+        this.message = message;
+    }
+}
+
+export const ErrorMiddleware = (
+    error: HttpExeception,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const status: number = error.status || 500;
+        const message: string = error.message || "Something went wrong";
+
+        res.status(status).send({
+            message,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
